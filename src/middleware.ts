bd0b407cee68a6728c2b02053e1247af636f6859
sayanChaterjee <chatterjeesayan1009@gmail.com>
2025-04-20 01:14:48 +0530

    message

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,7 +4,7 @@ const isPublicRoute = createRouteMatcher([
   '/sign-in(.*)',
   '/sign-up(.*)',
   '/',
-  '/api/webhooks/register(.*)'
+  'https://nebula-taupe-six.vercel.app/api/webhooks/register(.*)'
 ])
 
 export default clerkMiddleware(async (auth, req) => {
