d8f9fcc65f825cde321112be23c5b74d34d15ef3
sayanChaterjee <chatterjeesayan1009@gmail.com>
2025-04-19 21:47:57 +0530

    fixes

--- src/app/api/allrepos/route.ts
+++ src/app/api/allrepos/route.ts
@@ -6,39 +6,39 @@ import User from "@/models/User";
 import { auth } from "@clerk/nextjs/server";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(request: NextRequest, response : NextResponse) {
+export async function GET(request: NextRequest) {
   await dbconnect();
 
   try {
-    const {userId} : {userId : string | null | undefined} = await auth();
+    const { userId }: { userId: string | null | undefined } = await auth();
     
     if (!userId) {
-    throw new Error('Not authenticated');
+      return NextResponse.json({ success: false, error: 'Not authenticated' }, { status: 401 });
     }
     console.log(userId);
 
     const user = await User.findOne({ clerkId: userId });
     if (!user) {
-        throw new Error('User not found in database');
+      return NextResponse.json({ success: false, error: 'User not found in database' }, { status: 404 });
     }
     
-    //checking if the repo summary already exists
+    // Checking if the repo summary already exists
     const allrepos = await RepoSummaryModel.find({
-        userId : user.clerkId,
+      userId: user.clerkId,
     }).select('repoUrl');
 
-    if(!allrepos || allrepos.length === 0) {
-        return NextResponse.json({success : false, message : "Repo summary does not exist in your search history"}, {status : 200})
+    if (!allrepos || allrepos.length === 0) {
+      return NextResponse.json({ success: false, message: "Repo summary does not exist in your search history" }, { status: 200 });
     }
 
     const allreposUrls = allrepos.map((repo) => repo.repoUrl);
-    console.log("All repos : ", allreposUrls);
+    console.log("All repos:", allreposUrls);
 
-    return NextResponse.json({success : true, message : "Latest contents fetched successfully", data : allreposUrls}, {status : 200});
+    return NextResponse.json({ success: true, message: "Latest contents fetched successfully", data: allreposUrls }, { status: 200 });
 
   } catch (error: any) {
-    console.error('Error in POST /api/readme-content:', error);
+    console.error('Error in GET /api/readme-content:', error);
     const errorMessage = error instanceof Error ? error.message : 'An unknown error occurred';
-    return NextResponse.json({ success : false, error: errorMessage }, { status: 500 });
+    return NextResponse.json({ success: false, error: errorMessage }, { status: 500 });
   }
 }
