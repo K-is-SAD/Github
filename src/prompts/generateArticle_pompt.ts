86f61c1f6ede118efa971f81ea0c693771aecf40
Krishanu2206 <krishanub03@gmail.com>
2025-04-19 17:59:20 +0530

    message

--- src/prompts/generateArticle_pompt.ts
+++ src/prompts/generateArticle_pompt.ts
@@ -6,7 +6,7 @@ import { fileURLToPath } from 'url'
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
-const templatePath = path.resolve(__dirname, '../formats/articleTemplate.txt')
+const templatePath = path.resolve(__dirname, '../formats/articleTemplate.md')
 //console.log(templatePath)
 const template = fs.readFileSync(templatePath, 'utf-8');
 
