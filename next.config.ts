83764b4f13f52e3c118e83b3e3cd6ac178520fb4
K-is-SAD <krishanubiswas573@gmail.com>
2025-05-03 22:16:36 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   experimental: {
-    allowedDevOrigins: ['http://192.168.0.120'],
+    allowedDevOrigins: ['http://192.168.0.120', 'https://nebula-backend-1-pxku.onrender.com/'],
   },
-  crossOrigin:'anonymous',
+  crossOrigin:'use-credentials',
   images: {
     domains: ['img.clerk.com']
   }
